c19b07022a2a1225d61171903f76c14dd9ab5ac5
Shiv Kumar <shivyk12@gmail.com>
2021-02-28 01:19:48 -0500

    test

--- my-fifth-game/js/main.js
+++ my-fifth-game/js/main.js
@@ -1,64 +1,100 @@
 import "./phaser.js";
 
-// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
-// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
-// `phaser-example` to `game`, which is the id of the HTML element where we
-// want the game to go.
-// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
-// You will need to change the paths you pass to `this.load.image()` or any other
-// loading functions to reflect where you are putting the assets.
-// All loading functions will typically all be found inside `preload()`.
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
+var config = {
+    type: Phaser.AUTO,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    physics: {
+        default: 'arcade'
+    }
+};
 
-// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+//================================================================================
 
-class MyScene extends Phaser.Scene {
-    
-    constructor() {
-        super();
-        
-        this.bouncy = null;
+class Ship extends Phaser.GameObjects.Sprite  {
+
+    constructor(scene, x , y) {
+        super(scene, x, y);
+        this.setTexture('ship');
+        this.setPosition(x, y);
+
+        this.scene = scene;
+        this.deltaX = 5;
+        this.deltaY = 5;
     }
-    
-    preload() {
-        // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/phaser.png' );
+
+    moveLeft() {
+        if (this.x > 0) {
+            this.x -= this.deltaX;
+        }
     }
-    
-    create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-        
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Build something amazing.", style );
-        text.setOrigin( 0.5, 0.0 );
+
+    moveRight() {
+        if (this.x < SCREEN_WIDTH) {
+            this.x += this.deltaX;
+        }
     }
-    
-    update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
+
+    moveUp() {
+        if (this.y > 0) {
+            this.y -= this.deltaY;
+        }
+    }
+
+    moveDown() {
+
+        if (this.y < SCREEN_HEIGHT) {
+            this.y += this.deltaY;
+        }
+    }
+
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
     }
 }
 
-const game = new Phaser.Game({
-    type: Phaser.AUTO,
-    parent: 'game',
-    width: 800,
-    height: 600,
-    scene: MyScene,
-    physics: { default: 'arcade' },
-    });
+//================================================================================
+
+class Scene1 extends Phaser.Scene {
+
+    constructor(config) {
+        super(config);
+    }
+
+    preload() {
+        this.load.image('ship', 'assets/SpaceShooterRedux/PNG/playerShip1_orange.png');
+    }
+
+    create() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.myShip = new Ship(this, 400, 500);
+        this.add.existing(this.myShip);
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.myShip.moveLeft();
+        }
+
+        if (this.cursors.right.isDown) {
+            this.myShip.moveRight();
+        }
+
+        if (this.cursors.up.isDown) {
+            this.myShip.moveUp();
+        }
+
+        if (this.cursors.down.isDown) {
+            this.myShip.moveDown();
+        }
+
+        if (this.cursors.space.isDown) {
+            // shooting guns goes here
+        }
+    }
+}
+
+var game = new Phaser.Game(config);
+game.scene.add('scene1', Scene1, true, { x: 400, y: 300 });
