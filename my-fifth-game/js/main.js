5c131875d8b0f3aa8384d29f5a2a231807fd096f
Shiv Kumar <shivyk12@gmail.com>
2021-02-28 01:24:25 -0500

    test

--- my-fifth-game/js/main.js
+++ my-fifth-game/js/main.js
@@ -1,5 +1,6 @@
 import "./phaser.js";
 
+
 var SCREEN_WIDTH = 800;
 var SCREEN_HEIGHT = 600;
 var config = {
@@ -13,9 +14,9 @@ var config = {
 
 //================================================================================
 
-class Ship extends Phaser.GameObjects.Sprite  {
+class Ship extends Phaser.GameObjects.Sprite {
 
-    constructor(scene, x , y) {
+    constructor(scene, x, y) {
         super(scene, x, y);
         this.setTexture('ship');
         this.setPosition(x, y);
@@ -23,6 +24,9 @@ class Ship extends Phaser.GameObjects.Sprite  {
         this.scene = scene;
         this.deltaX = 5;
         this.deltaY = 5;
+        this.lasers = new Array();
+        this.lastShot = new Date().getTime();
+        this.shotFrequency = 250;
     }
 
     moveLeft() {
@@ -50,8 +54,117 @@ class Ship extends Phaser.GameObjects.Sprite  {
         }
     }
 
+    fireLasers() {
+        var currentTime = new Date().getTime();
+        if (currentTime - this.lastShot > this.shotFrequency) {
+            var shipLaser = new ShipLaser(this.scene, this.x, this.y);
+            this.scene.add.existing(shipLaser);
+            this.lasers.push(shipLaser);
+            this.lastShot = currentTime;
+        }
+    }
+
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
+
+        var i = 0;
+        var j = 0;
+        var lasersToRemove = new Array();
+
+        for (i = 0; i < this.lasers.length; i++) {
+            this.lasers[i].update();
+
+            if (this.lasers[i].y <= 0) {
+                lasersToRemove.push(this.lasers[i]);
+            }
+        }
+
+        for (j = 0; j < lasersToRemove.length; j++) {
+            var laserIndex = this.lasers.indexOf(lasersToRemove[j]);
+            this.lasers.splice(laserIndex, 1);
+            lasersToRemove[j].destroy();
+        }
+    }
+}
+
+//================================================================================
+
+class ShipLaser extends Phaser.GameObjects.Sprite {
+
+    constructor(scene, x, y) {
+        super(scene, x, y);
+        this.setTexture('laser');
+        this.setPosition(x, y);
+        this.speed = 10;
+        this.scene = scene;
+        scene.physics.world.enable(this);
+        scene.physics.add.collider(this, scene.enemies, this.handleHit, null, this);
+    }
+
+    handleHit(laserSprite, enemySprite) {
+        enemySprite.destroy(true);
+        laserSprite.destroy(true);
+    }
+
+    preUpdate(time, delta) {
+        if (this.active == false) { return; }
+        super.preUpdate(time, delta);
+        this.y -= this.speed;
+    }
+}
+
+//================================================================================
+
+class Enemy1 extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y);
+        this.setTexture('enemy1');
+        this.setPosition(x, y);
+        scene.physics.world.enable(this);
+
+        this.gameObject = this;
+        this.deltaX = 3;
+        this.deltaY = 3;
+    }
+
+    update() {
+        let k = Math.random() * 4;
+        k = Math.round(k);
+
+        if (k == 0) {
+            //this.moveUp();
+        }
+        else if (k == 2) {
+            this.moveLeft();
+        }
+        else if (k == 3) {
+            this.moveRight();
+        }
+    }
+
+    moveLeft() {
+        if (this.x > 0) {
+            this.x -= this.deltaX;
+        }
+    }
+
+    moveRight() {
+        if (this.x < SCREEN_WIDTH) {
+            this.x += this.deltaX;
+        }
+    }
+
+    moveUp() {
+        if (this.y > 0) {
+            this.y -= this.deltaY;
+        }
+    }
+
+    moveDown() {
+
+        if (this.y < SCREEN_HEIGHT) {
+            this.y += this.deltaY;
+        }
     }
 }
 
@@ -65,12 +178,29 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image('ship', 'assets/SpaceShooterRedux/PNG/playerShip1_orange.png');
+        this.load.image('laser', 'assets/SpaceShooterRedux/PNG/Lasers/laserBlue01.png');
+        this.load.image('enemy1', 'assets/SpaceShooterRedux/PNG/Enemies/enemyBlack3.png');
     }
 
     create() {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.myShip = new Ship(this, 400, 500);
         this.add.existing(this.myShip);
+        this.enemies = this.physics.add.group();
+        this.enemies2 = new Array();
+
+
+        let k = 0;
+        for (k = 0; k < 21; k++) {
+            let x = Math.random() * 800;
+            let y = Math.random() * 400;
+
+            this.enemy = new Enemy1(this, x, y);
+            this.add.existing(this.enemy);
+            this.enemies.add(this.enemy);
+            this.enemies2.push(this.enemy);
+        }
+
     }
 
     update() {
@@ -91,7 +221,15 @@ class Scene1 extends Phaser.Scene {
         }
 
         if (this.cursors.space.isDown) {
-            // shooting guns goes here
+            this.myShip.fireLasers();
+        }
+
+        this.myShip.update();
+
+        let j = 0;
+        for (j = 0; j < this.enemies2.length; j++) {
+            let enemy = this.enemies2[j];
+            enemy.update();
         }
     }
 }
