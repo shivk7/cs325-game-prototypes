ae2aac46de54379372a66774767f0adb536070d7
Shiv Kumar <shivyk12@gmail.com>
2021-02-28 13:20:22 -0500

    test 1

--- my-fifth-game/js/main.js
+++ my-fifth-game/js/main.js
@@ -4,7 +4,7 @@ const config = {
     type: Phaser.AUTO,
     parent: 'game',
     width: 800,
-    heigth: 640,
+    heigth: 600,
     scale: {
         mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH
