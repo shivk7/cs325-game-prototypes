3657223fdff6410900d02f4b7e089801e841acce
Shiv Kumar <shivyk12@gmail.com>
2021-03-10 11:20:57 -0500

    V1.1

--- my-sixth-game/js/main.js
+++ my-sixth-game/js/main.js
@@ -1,56 +1,136 @@
-"use strict";
-
-// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
-// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
-// `phaser-example` to `game`, which is the id of the HTML element where we
-// want the game to go.
-// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
-// You will need to change the paths you pass to `this.load.image()` or any other
-// loading functions to reflect where you are putting the assets.
-// All loading functions will typically all be found inside `preload()`.
-
-function preload() {
-    // Load an image and call it 'logo'.
-    this.load.image( 'logo', 'assets/phaser.png' );
-}
-
-let bouncy;
-
-function create() {
-    // Create a sprite at the center of the screen using the 'logo' image.
-    bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-    
-    // Make it bounce off of the world bounds.
-    bouncy.body.collideWorldBounds = true;
-    
-    // Make the camera shake when clicking/tapping on it.
-    bouncy.setInteractive();
-    bouncy.on( 'pointerdown', function( pointer ) {
-        this.scene.cameras.main.shake(500);
-        });
-    
-    // Add some text using a CSS style.
-    // Center it in X, and position its top 15 pixels from the top of the world.
-    let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-    let text = this.add.text( this.cameras.main.centerX, 15, "Build something amazing.", style );
-    text.setOrigin( 0.5, 0.0 );
-}
-
-function update() {
-    // Accelerate the 'logo' sprite towards the cursor,
-    // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-    // in X or Y.
-    // This function returns the rotation angle that makes it visually match its
-    // new trajectory.
-    bouncy.rotation = this.physics.accelerateToObject( bouncy, this.input.activePointer, 500, 500, 500 );
-}
-
-const game = new Phaser.Game({
+var config = {
     type: Phaser.AUTO,
-    parent: 'game',
     width: 800,
     height: 600,
-    // Phaser converts a dictionary of methods into a Scene subclass.
-    scene: { preload: preload, create: create, update: update },
-    physics: { default: 'arcade' },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 500 },
+            debug: false
+        }
+    },
+    scene: {
+        key: 'main',
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var map;
+var player;
+var cursors;
+var groundLayer, coinLayer;
+var text;
+var score = 0;
+
+function preload() {
+    // map made with Tiled in JSON format
+    this.load.tilemapTiledJSON('map', 'assets/map.json');
+    // tiles in spritesheet 
+    this.load.spritesheet('tiles', 'assets/tiles.png', { frameWidth: 70, frameHeight: 70 });
+    // simple coin image
+    this.load.image('coin', 'assets/coinGold.png');
+    // player animations
+    this.load.atlas('player', 'assets/player.png', 'assets/player.json');
+}
+
+function create() {
+    // load the map 
+    map = this.make.tilemap({ key: 'map' });
+
+    // tiles for the ground layer
+    var groundTiles = map.addTilesetImage('tiles');
+    // create the ground layer
+    groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
+    // the player will collide with this layer
+    groundLayer.setCollisionByExclusion([-1]);
+
+    // coin image used as tileset
+    var coinTiles = map.addTilesetImage('coin');
+    // add coins as tiles
+    coinLayer = map.createDynamicLayer('Coins', coinTiles, 0, 0);
+
+    // set the boundaries of our game world
+    this.physics.world.bounds.width = groundLayer.width;
+    this.physics.world.bounds.height = groundLayer.height;
+
+    // create the player sprite    
+    player = this.physics.add.sprite(200, 200, 'player');
+    player.setBounce(0.2); // our player will bounce from items
+    player.setCollideWorldBounds(true); // don't go out of the map    
+
+    // small fix to our player images, we resize the physics body object slightly
+    player.body.setSize(player.width, player.height - 8);
+
+    // player will collide with the level tiles 
+    this.physics.add.collider(groundLayer, player);
+
+    coinLayer.setTileIndexCallback(17, collectCoin, this);
+    // when the player overlaps with a tile with index 17, collectCoin 
+    // will be called    
+    this.physics.add.overlap(player, coinLayer);
+
+    // player walk animation
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNames('player', { prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2 }),
+        frameRate: 10,
+        repeat: -1
     });
+    // idle with only one frame, so repeat is not neaded
+    this.anims.create({
+        key: 'idle',
+        frames: [{ key: 'player', frame: 'p1_stand' }],
+        frameRate: 10,
+    });
+
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // set bounds so the camera won't go outside the game world
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    // make the camera follow the player
+    this.cameras.main.startFollow(player);
+
+    // set background color, so the sky is not black    
+    this.cameras.main.setBackgroundColor('#ccccff');
+
+    // this text will show the score
+    text = this.add.text(20, 570, '0', {
+        fontSize: '20px',
+        fill: '#ffffff'
+    });
+    // fix the text to the camera
+    text.setScrollFactor(0);
+}
+
+// this function will be called when the player touches a coin
+function collectCoin(sprite, tile) {
+    coinLayer.removeTileAt(tile.x, tile.y); // remove the tile/coin
+    score++; // add 10 points to the score
+    text.setText(score); // set the text to show the current score
+    return false;
+}
+
+function update(time, delta) {
+    if (cursors.left.isDown) {
+        player.body.setVelocityX(-200);
+        player.anims.play('walk', true); // walk left
+        player.flipX = true; // flip the sprite to the left
+    }
+    else if (cursors.right.isDown) {
+        player.body.setVelocityX(200);
+        player.anims.play('walk', true);
+        player.flipX = false; // use the original sprite looking to the right
+    } else {
+        player.body.setVelocityX(0);
+        player.anims.play('idle', true);
+    }
+    // jump 
+    if (cursors.up.isDown && player.body.onFloor()) {
+        player.body.setVelocityY(-500);
+    }
+}
