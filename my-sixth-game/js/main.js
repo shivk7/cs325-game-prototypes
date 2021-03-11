b73415b67b93b528db4805873b0f36962b2a7b7d
Shiv Kumar <shivyk12@gmail.com>
2021-03-11 10:40:58 -0500

    V1

--- my-sixth-game/js/main.js
+++ my-sixth-game/js/main.js
@@ -18,13 +18,13 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-var player;
+
 var cursors;
 function preload() {
     this.load.image('coins', 'assets/coinGold.png');
     this.load.image('tiles', 'assets/house tiles.png');
     this.load.tilemapTiledJSON('map', 'assets/map.json');
-    this.load.spritesheet('player', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.atlas('player', 'assets/kenney_player.png', 'assets/kenney_player_atlas.json');
 }
 function create() {
     const map = this.make.tilemap({ key: 'map' });
@@ -37,63 +37,46 @@ function create() {
     this.physics.world.bounds.width = 1600;
     this.physics.world.bounds.height = 800;
 
-    player = this.physics.add.sprite(50, 300, 'player');
-    player.setBounce(0.1);
-    player.setCollideWorldBounds(true);
-    this.physics.add.collider(player, platform);
+    this.player = this.physics.add.sprite(50, 300, 'player');
+    this.player.setBounce(0.1);
+    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, platform);
 
     this.cameras.main.setBounds(0, 0, 1600, 600);
-    this.cameras.main.startFollow(player);
+    this.cameras.main.startFollow(this.player);
     this.cameras.main.setBackgroundColor('#ccccff'); 
 
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNames('player', {
+            prefix: 'robo_player_',
+            start: 2,
+            end: 3,
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // Create an idle animation i.e the first frame
+    this.anims.create({
+        key: 'idle',
+        frames: [{ key: 'player', frame: 'robo_player_0' }],
+        frameRate: 10,
+    });
+
+    // Use the second frame of the atlas for jumping
+    this.anims.create({
+        key: 'jump',
+        frames: [{ key: 'player', frame: 'robo_player_1' }],
+        frameRate: 10,
+    });
+
     this.cursors = this.input.keyboard.createCursorKeys();
-
-    //Gives player turning left animations
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    })
-
-    //Gives player turn animations
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
-        frameRate: 10
-    });
-
-    //Gives player truning right animations
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
+    
 
 }
 
 function update() {
-    cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-        player.setVelocityX(-600);
-
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown) {
-        player.setVelocityX(600);
-
-        player.anims.play('right', true);
-    }
-    else {
-        player.setVelocityX(0);
-
-        player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(600);
-    }
+  
     
 }
