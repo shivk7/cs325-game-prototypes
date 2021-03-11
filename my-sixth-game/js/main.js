b81755e6e93ce6d116bc162d7f573a45c1a7ecf6
shivy <shivy@DESKTOP-T8QHF8H>
2021-03-10 23:53:27 -0500

    V1

--- my-sixth-game/js/main.js
+++ my-sixth-game/js/main.js
@@ -31,7 +31,7 @@ function preload() {
 function create() {
     const map = this.make.tilemap({ key: 'map' });
     const tileset = map.addTilesetImage('house tiles', 'tiles');
-    const platforms = map.createStaticLayer('platform', tileset, 0, 200);
+    const platforms = map.createStaticLayer('platform', tileset, 0, 100);
 }
 
 function update() {
