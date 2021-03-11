7d2347e1b6968c547d2b159d6a17ce5d761bb8ab
Shiv Kumar <shivyk12@gmail.com>
2021-03-11 09:27:58 -0500

    V1

--- my-sixth-game/js/main.js
+++ my-sixth-game/js/main.js
@@ -36,6 +36,30 @@ function create() {
     this.player.setBounce(0.1);
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, platform);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'player', frame: 4 }],
+        frameRate: 10
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+
 }
 
 function update() {
