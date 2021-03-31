86e51d7e50e8496163771539193e0037e8e279be
Shiv Kumar <shivyk12@gmail.com>
2021-03-31 16:45:08 -0400

    test

--- my-seventh-game/js/main.js
+++ my-seventh-game/js/main.js
@@ -1,3 +1,18 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: Demo,
+    backgroundColor: 0x33333,
+};
+
+var game = new Phaser.Game(config);
+
 class Demo extends Phaser.Scene {
     constructor() {
         super({
@@ -48,18 +63,3 @@ var CreateCard = function (scene, x, y, frontFace) {
             // this.flip.flip();
         })
 }
-
-var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    scene: Demo,
-    backgroundColor: 0x33333,
-};
-
-var game = new Phaser.Game(config);
