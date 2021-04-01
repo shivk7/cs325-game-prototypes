115f5ecbd6d6fa40ea26ef73ba10a019ce97ee05
Shiv Kumar <shivyk12@gmail.com>
2021-03-31 23:21:55 -0400

    test

--- my-seventh-game/js/main.js
+++ my-seventh-game/js/main.js
@@ -1,87 +1,52 @@
-// the game itself
-var game;
+import "./phaser.js";
 
-// global object with game options
-var gameOptions = {
-
-    // flipping speed in milliseconds
-    flipSpeed: 200,
-
-    // flipping zoom ratio. Simulates the card to be raised when flipping
-    flipZoom: 1.2
-}
-window.onload = function() {
-
-    // creation of a 500x500 pixels game
-    game = new Phaser.Game(500, 500);
-
-    // game states
-    game.state.add("PlayGame", playGame);
-    game.state.start("PlayGame");
-}
-
-var playGame = function(game){}
-playGame.prototype = {
-    preload: function(){
-
-        // making the game cover the biggest window area possible while showing all content
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-
-        // changing background colors
-        game.stage.backgroundColor = 0x448844;
-
-        // card sprite sheet
-        game.load.spritesheet("cards", "cards.png", 167, 243);
-
-    },
-    create: function(){
-
-        // adding the card
-        this.card =  game.add.sprite(game.width / 2, game.height / 2, "cards", 0);
-
-        // setting card anchor points to its center
-        this.card.anchor.set(0.5);
-
-        // custom property to tell us if the card is flipping. It's not, at the moment.
-        this.card.isFlipping = false;
-
-        // waiting for player input
-        game.input.onDown.add(function(){
-
-            // if the card is not flipping...
-            if(!this.card.isFlipping){
-
-                // it's flipping now!
-                this.card.isFlipping = true;
-
-                // start the first of the two flipping animations
-                this.flipTween.start();
-            }
-        }, this);
-
-        // first tween: we raise and flip the card
-        this.flipTween = game.add.tween(this.card.scale).to({
-            x: 0,
-            y: gameOptions.flipZoom
-        }, gameOptions.flipSpeed / 2, Phaser.Easing.Linear.None);
-
-        // once the card is flipped, we change its frame and call the second tween
-        this.flipTween.onComplete.add(function(){
-            this.card.frame = 1 - this.card.frame;
-            this.backFlipTween.start();
-        }, this);
-
-        // second tween: we complete the flip and lower the card
-        this.backFlipTween = game.add.tween(this.card.scale).to({
-            x: 1,
-            y: 1
-        }, gameOptions.flipSpeed / 2, Phaser.Easing.Linear.None);
-
-        // once the card has been placed down on the table, we can flip it again
-        this.backFlipTween.onComplete.add(function(){
-            this.card.isFlipping = false;
-        }, this);
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
+
+var game = new Phaser.Game(config);
+
+var card1
+var card2
+function preload() {
+    this.load.image('card1', 'assets/cardBack.png')
+    this.load.image('cat', 'assets/cat.png')
+}
+
+function create() {
+    this.add.image(400, 300, 'cat')
+    this.add.image(500, 300, 'cat')
+
+    card1 = this.add.image(400, 300, 'card1');
+    card2 = this.add.image(500, 300, 'card2');
+
+    this.input.once('pointerdown', function () {
+
+        console.log('nuked');
+
+        card1.destroy();
+
+        card1 = null;
+
+    });
+
+    this.input.once('pointerdown', function () {
+
+        console.log('nuked');
+
+        card2.destroy();
+
+        card2 = null;
+
+    });
+}
+
+function update() {
 }
