d437cad5279ef8880b0e0d13742a1472dc59558e
Shiv Kumar <shivyk12@gmail.com>
2021-03-31 23:44:25 -0400

    test

--- my-seventh-game/js/main.js
+++ my-seventh-game/js/main.js
@@ -13,29 +13,41 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-
+var card1
+var card2
 function preload() {
     this.load.image('card1', 'assets/cardBack.png')
-    this.load.image('card2', 'assets/cardBack.png')
+    this.load.image('card2', 'assets/cardBack1.png')
     this.load.image('cat', 'assets/cat.png')
 }
 
 function create() {
-    this.add.image(300, 300, 'cat')
-    this.add.image(600, 300, 'cat')
+    this.add.image(400, 300, 'cat')
+    this.add.image(500, 300, 'cat')
 
-    this.card1 = this.add.image(300, 300, 'card1');
-    this.card2 = this.add.image(600, 300, 'card2');
+    card1 = this.add.image(400, 300, 'card1');
+    card2 = this.add.image(500, 300, 'card2');
 
-    this.card1.setInteractive();
-    this.card2.setInteractive()
+    this.input.once('pointerdown', function () {
 
-    this.input.on('gameobjectdown', this.onClicked.bind(this));
-    onClicked(pointer, objectClicked);{
-        objectClicked.destroy();
+        console.log('nuked');
 
+        card1.remove();
 
-    }
+        card1 = null;
+
+    });
+
+    this.input.once('pointerdown', function () {
+
+        console.log('nuked');
+
+        card2.remove();
+
+        card1 = null;
+
+    });
 }
-    function update() {
-    }
+
+function update() {
+}
