a52f716d937fe392d941847abf7e783f1223b495
Shiv Kumar <shivyk12@gmail.com>
2021-04-01 00:19:21 -0400

    test

--- my-seventh-game/js/main.js
+++ my-seventh-game/js/main.js
@@ -1,41 +1,8 @@
-import "./phaser.js";
-
-var config = {
+let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    parent: 'phaser-game',
+    scene: [SceneMain]
 };
-
-var game = new Phaser.Game(config);
-
-
-function preload() {
-    this.load.image('card1', 'assets/cardBack.png')
-    this.load.image('card2', 'assets/cardBack.png')
-    this.load.image('cat', 'assets/cat.png')
-}
-
-function create() {
-    this.add.image(300, 300, 'cat')
-    this.add.image(600, 300, 'cat')
-
-    this.card1 = this.add.image(300, 300, 'card1');
-    this.card2 = this.add.image(600, 300, 'card2');
-
-    this.card1.setOrigin(0.5, 0.5);
-    this.card2.setOrigin(0.5, 0.5);
-
-    this.input.on('gameobjectdown', this.onClicked.bind(this));
-}
-onClicked(pointer, objectClicked);{
-    objectClicked.destroy();
-}
-
-
-function update() {
-}
+let game = new Phaser.Game(config);
