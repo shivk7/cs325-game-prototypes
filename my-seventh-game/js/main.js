965b064e9bc22b1653711e375505cc843992c4f6
shivy <shivy@DESKTOP-T8QHF8H>
2021-04-07 23:33:16 -0400

    test

--- my-seventh-game/js/main.js
+++ my-seventh-game/js/main.js
@@ -15,18 +15,26 @@ var game = new Phaser.Game(config);
 
 
 function preload() {
-    this.load.image('card1', 'assets/cardBack.png')
-    this.load.image('card2', 'assets/cardBack.png')
-    this.load.image('card3', 'assets/cardBack.png')
-    this.load.image('card4', 'assets/cardBack.png')
-    this.load.image('card5', 'assets/cardBack.png')
-    this.load.image('card6', 'assets/cardBack.png')
-    this.load.image('jj', 'assets/jumping_jacks.png')
-    this.load.image('PU', 'assets/push_ups.png')
-    this.load.image('SQ', 'assets/squats.png')
-    this.load.image('5', 'assets/5.png')
-    this.load.image('50', 'assets/50.png')
-    this.load.image('10', 'assets/10.png')
+    this.load.image('card1', 'assets/cardBack.png');
+    this.load.image('card2', 'assets/cardBack.png');
+    this.load.image('card3', 'assets/cardBack.png');
+    this.load.image('card4', 'assets/cardBack.png');
+    this.load.image('card5', 'assets/cardBack.png');
+    this.load.image('card6', 'assets/cardBack.png');
+    this.load.image('jj', 'assets/jumping_jacks.png');
+    this.load.image('PU', 'assets/push_ups.png');
+    this.load.image('SQ', 'assets/squats.png');
+    this.load.image('5', 'assets/5.png');
+    this.load.image('50', 'assets/50.png');
+    this.load.image('100', 'assets/100.png');
+    this.load.image('15', 'assets/15.png');
+    this.load.image('20', 'assets/20.png');
+    this.load.image('25', 'assets/25.png');
+    this.load.image('30', 'assets/30.png');
+    this.load.image('bps', 'assets/burpees.png');
+    this.load.image('ll', 'assets/leg_lifts.png');
+    this.load.image('lgs', 'assets/lunges.png');
+    this.load.image('mc', 'assets/mountain_climbers.png');
 }
 
 function create() {
@@ -36,7 +44,7 @@ function create() {
 
     this.add.image(100, 500, '5')
     this.add.image(400, 500, '50')
-    this.add.image(700, 500, '10')
+    this.add.image(700, 500, '100')
 
 
     this.card1 = this.add.image(100, 100, 'card1');
