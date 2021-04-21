76ca49fecbd2fdd8afc9d8704a87417ca4312df8
Shiv Kumar <shivyk12@gmail.com>
2021-04-21 11:41:01 -0400

    test

--- my-eighth-game/js/main.js
+++ my-eighth-game/js/main.js
@@ -2,7 +2,7 @@ import "./phaser.js";
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 800,
     scene: {
         preload: preload,
         create: create,
@@ -14,10 +14,18 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.spritesheet('dice', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('dice2', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('dice3', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('dice4', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('dice5', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
 }
 
 function create() {
-    const dice = this.add.sprite(200, 300, 'dice')
+    const dice = this.add.sprite(400, 300, 'dice')
+    const dice = this.add.sprite(600, 300, 'dice2')
+    const dice = this.add.sprite(200, 300, 'dice3')
+    const dice = this.add.sprite(300, 600, 'dice4')
+    const dice = this.add.sprite(500, 600, 'dice5')
 
     this.input.on('pointerdown', () => {
         this.tweens.addCounter({
@@ -30,6 +38,60 @@ function create() {
         });
     });
 
+    this.input.on('pointerdown', () => {
+        this.tweens.addCounter({
+            duration: 200,
+            repeat: 10,
+            onRepeat: () => {
+                const frame = Phaser.Math.Between(1, 6);
+                dice1.setFrame(frame);
+            }
+        });
+    });
+
+    this.input.on('pointerdown', () => {
+        this.tweens.addCounter({
+            duration: 200,
+            repeat: 10,
+            onRepeat: () => {
+                const frame = Phaser.Math.Between(1, 6);
+                dice2.setFrame(frame);
+            }
+        });
+    });
+
+    this.input.on('pointerdown', () => {
+        this.tweens.addCounter({
+            duration: 200,
+            repeat: 10,
+            onRepeat: () => {
+                const frame = Phaser.Math.Between(1, 6);
+                dice3.setFrame(frame);
+            }
+        });
+    });
+
+    this.input.on('pointerdown', () => {
+        this.tweens.addCounter({
+            duration: 200,
+            repeat: 10,
+            onRepeat: () => {
+                const frame = Phaser.Math.Between(1, 6);
+                dice4.setFrame(frame);
+            }
+        });
+    });
+
+    this.input.on('pointerdown', () => {
+        this.tweens.addCounter({
+            duration: 200,
+            repeat: 10,
+            onRepeat: () => {
+                const frame = Phaser.Math.Between(1, 6);
+                dice5.setFrame(frame);
+            }
+        });
+    });
 }
 
 function update() {
