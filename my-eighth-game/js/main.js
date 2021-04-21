ef9b89d7a3c6b02a3bf473c22028c2b034a22012
Shiv Kumar <shivyk12@gmail.com>
2021-04-21 12:04:40 -0400

    test

--- my-eighth-game/js/main.js
+++ my-eighth-game/js/main.js
@@ -15,29 +15,29 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.spritesheet('dice', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
 
+}
+    function create() {
+        const dice = this.add.sprite(400, 300, 'dice')
+        const dice2 = this.add.sprite(600, 300, 'dice')
+        const dice3 = this.add.sprite(200, 300, 'dice')
+        const dice4 = this.add.sprite(300, 600, 'dice')
+        const dice5 = this.add.sprite(500, 600, 'dice')
 
-function create() {
-    const dice = this.add.sprite(400, 300, 'dice')
-    const dice2 = this.add.sprite(600, 300, 'dice')
-    const dice3 = this.add.sprite(200, 300, 'dice')
-    const dice4 = this.add.sprite(300, 600, 'dice')
-    const dice5 = this.add.sprite(500, 600, 'dice')
-
-    this.input.on('pointerdown', () => {
-        this.tweens.addCounter({
-            duration: 200,
-            repeat: 10,
-            onRepeat: () => {
-                dice1.setFrame(Phaser.Math.Between(0, 5));
-                dice2.setFrame(Phaser.Math.Between(0, 5));
-                dice3.setFrame(Phaser.Math.Between(0, 5));
-                dice4.setFrame(Phaser.Math.Between(0, 5));
-                dice5.setFrame(Phaser.Math.Between(0, 5));
-            }
+        this.input.on('pointerdown', () => {
+            this.tweens.addCounter({
+                duration: 200,
+                repeat: 10,
+                onRepeat: () => {
+                    dice1.setFrame(Phaser.Math.Between(0, 5));
+                    dice2.setFrame(Phaser.Math.Between(0, 5));
+                    dice3.setFrame(Phaser.Math.Between(0, 5));
+                    dice4.setFrame(Phaser.Math.Between(0, 5));
+                    dice5.setFrame(Phaser.Math.Between(0, 5));
+                }
+            });
         });
-    });
 
-}
+    }
 
-function update() {
-}
+    function update() {
+    }
