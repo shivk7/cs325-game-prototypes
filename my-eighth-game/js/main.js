2ab112ee1e6f21ee37356c3e1920ee1b2e25a5e5
Shiv Kumar <shivyk12@gmail.com>
2021-04-22 10:14:50 -0400

    test

--- my-eighth-game/js/main.js
+++ my-eighth-game/js/main.js
@@ -15,17 +15,11 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.spritesheet('dice', 'assets/dice.png', { frameWidth: 64, frameHeight: 64 });
     this.load.spritesheet('math', 'assets/math.png', { frameWidth: 60, frameHeight: 55 });
-    this.load.spritesheet('answer', 'assets/answerButton.png', { frameWidth: 100, frameHeight: 40 });
 }
     function create() {
         const dice1 = this.add.sprite(400, 300, 'dice')
         const dice2 = this.add.sprite(600, 300, 'dice')
 
-        const answerButton = this.add.sprite(500, 500, 'answer')
-        answerButton.setInteractive();
-
-        answerButton.on('pointerdown', () => { /* do things here */ });
-        const math1 = this.add.sprite(500, 300, 'math')
         
 
         const value1 = Number(dice1.frame.name + 1);
