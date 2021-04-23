2f2c65e64aaca1ec918a2f995ddece1aec41fe3a
Shiv Kumar <shivyk12@gmail.com>
2021-04-23 10:35:44 -0400

    test

--- my-eighth-game/js/main.js
+++ my-eighth-game/js/main.js
@@ -78,6 +78,35 @@ function preload() {
         });
 
         player = this.physics.add.sprite(400, 350, 'dude');
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
         this.physics.add.collider(player, grasslayer);
         cursors = this.input.keyboard.createCursorKeys();
     }
